7c3916be76b392033a9d960dca3fd09ecf73bd80
pls <1256622856@qq.com>
2020-12-12 17:02:33 +0800

    fen_zhi

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -7,6 +7,7 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello_git");
+        System.out.println("hello_git_1");
         System.out.println("hello_git_2");
     }
 }
